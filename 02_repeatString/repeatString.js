7afa62d03af2b41f41d8d885184330a98147309d
vladlenth <vladbass445@gmail.com>
2024-10-26 14:06:03 +0300

    com

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(word,n) {
+    let result = "";
+    for (let i = 0; i < n; i++) {
+        result += word;
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = repeatString;
+
+//npm test repeatString.spec.js
