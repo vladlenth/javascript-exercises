e18ab90f6cc4b1f5b40f8a08494e48fdf0dee940
vladlenth <vladbass445@gmail.com>
2024-10-26 19:14:41 +0300

    comm

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    let i = 0;
+    while (i < arr.length) {
+        if (arr[i] === value) {
+            arr.splice(i,1);
+        } else {
+            ++i;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
