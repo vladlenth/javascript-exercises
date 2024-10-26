bbc92bf3fd1b2a01cc556e4182622d6142c04e61
vladlenth <vladbass445@gmail.com>
2024-10-26 18:45:07 +0300

    comm

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let words = str.split('').filter(word => word);
+    words.reverse();
+    return words.join('');
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+// const repeatString = function(word,n) {
+//     let result = "";
+//     for (let i = 0; i < n; i++) {
+//         result += word;
+//     }
+//     return result;
+// }
