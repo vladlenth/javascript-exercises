c9320df1349a5907abc3f93cf5592a6b91c133d1
vladlenth <vladbass445@gmail.com>
2024-11-03 14:47:06 +0300

    com

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let wordR = word.split("").reverse().join("");
+    if (wordR === word) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(count) {
+    const fib = [0, 1];
+    for (let i = 2; i <= count; i++) {
+       fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[count];}
 // Do not edit below this line
 module.exports = fibonacci;
